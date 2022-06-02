88f32c28d8ec2605015bd0f36bfd2f9197911aad
Ирина Романова <romanova_98@mail.ru>
2022-06-02 23:19:52 +0300

    last version

--- src/main/java/com/aplha/test/controllers/TestController.java
+++ src/main/java/com/aplha/test/controllers/TestController.java
@@ -3,20 +3,35 @@ package com.aplha.test.controllers;
 import com.aplha.test.services.GiphyService;
 import com.aplha.test.services.RateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
-@RestController("/api")
+@Controller
+//@RequestMapping("/api")
 public class TestController {
 
-    @Autowired
-    RateService rateService;
+//    @Autowired
+//    RateService rateService;
+//    @Autowired
+//    GiphyService giphyService;
+//
+//    @GetMapping("/api")
+//    //@RequestMapping("/api")
+//   // @ResponseBody
+//    public String test() {
+//        try {
+//            return "index.html";
+//            //giphyService.getUrl(rateService.getRate()));
+//
+//        } catch (Exception e) {
+//               return "";
+//        }
 
-    @Autowired
-    GiphyService giphyService;
-
-    @GetMapping("/test")
-        public String test() {
-            return "GIF URL - " + giphyService.getUrl(rateService.getRate()) ;
-        }
+        @GetMapping("/greeting")
+        public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+            model.addAttribute("name", name);
+            return "index";
+    }
 }
